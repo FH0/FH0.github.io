70b975210fa30a61279bfe2e02670115f7d62995
FH0 <clbcjt@gmail.com>
2019-12-10 11:32:23 +0000

    update

--- update_jekyll_serve.sh
+++ update_jekyll_serve.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 while true;do
-	bundle exec jekyll s --host 0.0.0.0 --port 808 &
+	cd /root/FH0.github.io
+	bundle exec jekyll s --host 0.0.0.0 --port 808 --incremental &
 	jekyll_pid=$!
 	read
 	kill $jekyll_pid
